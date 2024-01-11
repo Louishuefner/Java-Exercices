9f776ac016e8646c3c0a584a2e7209de37ed5c9f
Louis Hüfner <louis.huefner@maibornwolff.de>
2024-01-11 09:01:44 +0100

    change

--- ArraySorting.java
+++ ArraySorting.java
@@ -14,34 +14,45 @@ public class ArraySorting {
     }
 
     
-    public static int[] takeNumbers(Scanner arrays){
+    public static int[] takeNumbers(Scanner arrays) {
 
         System.out.print("\n\nEnter Elements with a ',' in between: ");
         String numbers = HelperMethods.forNumbers(arrays);
         String[] numberArray = numbers.split(",");
         int[] intArray = new int[numberArray.length];
-        for(int i = 0; i < numberArray.length; i++){
+        for (int i = 0; i < numberArray.length; i++) {
             intArray[i] = Integer.parseInt(numberArray[i]);
         }
-        for(int l = 0; l < intArray.length; l++){
-            for(int i = 0; i < intArray.length - 1; i++){
-            while(intArray[i] > intArray[i + 1]){
-                int temp = intArray[i];
-                intArray[i] = intArray[i+1];
-                intArray[i+1] = temp;
+
+        if (!isSorted(intArray)) {
+            for (int l = 0; l < intArray.length; l++) {
+                for (int i = 0; i < intArray.length - 1; i++) {
+                    while (intArray[i] > intArray[i + 1]) {
+                        int temp = intArray[i];
+                        intArray[i] = intArray[i + 1];
+                        intArray[i + 1] = temp;
+                    }
+                }
             }
-        }
         }
         return intArray;
     }
 
+    public static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-    public static void output(int[] intArray){
-        
+    public static void output(int[] intArray) {
+
         System.out.print("\nSorted Array : ");
-        for (int i = 0; i < intArray.length - 1; i++){
+        for (int i = 0; i < intArray.length - 1; i++) {
             System.out.print(intArray[i] + ", ");
         }
-        System.out.print(intArray[intArray.length - 1] + "\n");     
+        System.out.print(intArray[intArray.length - 1] + "\n");
     }
 }
